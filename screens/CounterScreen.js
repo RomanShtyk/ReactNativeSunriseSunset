39b9f9904b3d4df1c12d2d49dd3e1c6af58b7d5e
Roman Shtykalo <rdsh@ukr.net>
2019-08-13 09:19:28 +0300

    redux added

---
+++ screens/CounterScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Counter from "../components/Counter";
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
+
+export default class CounterScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+    };
+
+    render() {
+        return (
+            <Provider store={store}>
+                <Counter/>
+            </Provider>
+        );
+    }
+};
+
+const initialState = {
+    counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREASE_COUNTER':
+            return { counter: state.counter + 1 };
+        case 'DECREASE_COUNTER':
+            return { counter: state.counter - 1 }
+    }
+    return state
+};
+
+const store = createStore(reducer);
